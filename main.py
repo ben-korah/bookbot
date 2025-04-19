8d134151bf635ef61a0821812af7c72a1adaf57d
ben-korah <shanethesane@yahoo.com>
2025-04-19 11:14:23 -0500

    D: Sorted Character Printout

--- main.py
+++ main.py
@@ -6,8 +6,23 @@ def get_book_text(filepath):
 def main():
     #print(get_book_text("books/frankenstein.txt"))
     wordcount = count_words(get_book_text("books/frankenstein.txt"))
-    print(f"{wordcount} words found in the document")
+    #print(f"{wordcount} words found in the document")
+    charcount = count_chars(get_book_text("books/frankenstein.txt"))
+    #print(f"Character Dictionary: {charcount}")
+    sorted = sort_chars(get_book_text("books/frankenstein.txt"))
+    print('============ BOOKBOT ============')
+    print('Analyzing book found at books/frankenstein.txt...')
+    print('----------- Word Count ----------')
+    print(f'Found {wordcount} total words')
+    print('--------- Character Count -------')
+    for char in sorted:
+        if char['char'].isalpha():
+            print(f'{char['char']}: {char['num']}')
+    print('============= END ===============')
 
 from stats import count_words
+from stats import count_chars
+from stats import sort_chars
+
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,32 @@ def count_words(text):
     word_array = text.split()
     return len(word_array)
 
+def count_chars(text):
+    char_count = {}
+    uncased = text.lower()
+    #word_array = word_array.split()
+    for char in uncased:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
+    #return word_array
+def sort_on(dict):
+    return dict["num"]
+
+def  sort_chars(text):
+    chars = count_chars(text)
+    character = []
+    i = -1
+    for char in chars:
+        i += 1
+        character.append({'char': char, 'num': chars[char]})
+    character.sort(reverse=True, key=sort_on)
+    return character
+
+def test():
+    print(f'{sort_chars('This is a tEst')}')
+
+#test()
+
